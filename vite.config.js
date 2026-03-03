f1037851f90630555b7cfa2e7b4884bd407e486f
arnabdutta04 <arnabdutta453@email.com>
2026-03-03 11:29:35 +0530

    Fix: add .gitignore and update config files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Weather-Prediction-for-Sustainable-E-Agriculture/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
